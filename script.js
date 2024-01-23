9a8f839532d6053d568a4b9c177d7361ea9ff17e
Rosslaw08 <ralawson.rl@gmail.com>
2024-01-23 16:23:03 +0000

    Add work notes to script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+// Create getComputerChoice function
+    // choose Rock/Paper/Scissors at random
+    // return result in computerSelection
+
+// Create playerSelection variable
+    // prompt user input Rock/Paper/Scissors
+    // store input in playerSelection
+        // make sure result == Rock/Paper/Scissors, return error if wrong
+
+// Create playRound function
+    // if playerSelection loses to computerSelection return string "You Lose! x beats y"
+    // if playerSelection wins to computerSelection return string "You Win! x beats y"
+    // if TIE then replay the round
+
+
+// Create game function
+    // plays a best of 5 rounds that keeps score and reports overall winner or loser
+
+
+
+
